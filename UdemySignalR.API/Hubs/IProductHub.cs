1d2546b7af982779901c7bad532a55020b6cd1c2
Ali <aliyildizoz909@gmail.com>
2021-03-14 18:30:54 +0300

    Added Strongly type

---
+++ UdemySignalR.API/Hubs/IProductHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemySignalR.API.Models;
+
+namespace UdemySignalR.API.Hubs
+{
+    public interface IProductHub
+    {
+        Task ReceiveProduct(Product product);
+        Task ReceiveProduct2(Product product);
+        Task ReceiveProduct3(Product product);
+    }
+}
---
+++ UdemySignalR.API/Hubs/ProductHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using UdemySignalR.API.Hubs;
+using UdemySignalR.API.Models;
+
+namespace UdemySignalR.API.Hubs
+{
+    public class ProductHub : Hub<IProductHub>
+    {
+        public async Task SendProduct(Product product)
+        {
+            await Clients.All.ReceiveProduct(product);
+        }
+    }
+}
